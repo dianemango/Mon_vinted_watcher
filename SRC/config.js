0c63413f45677c00ade115751a8c6f0ed53393bc
dianemango <dianemercier@live.fr>
2025-07-30 23:39:33 -1000

    ajout config.js
    Ce sont les 3 catégories spécifiques avec URLS fictifs , donc il faudra adapter l’API (scraper) plus tard

---
+++ SRC/config.js
@@ -0,0 +1,17 @@
+export const categoriesConfig = [
+  {
+    id: "salopettes",
+    name: "Salopettes",
+    apiUrl: "https://api-vinted-fake.com/search?category=salopettes&brands=Dickies,Carhartt,Levi’s&price_min=15&price_max=50&region=Europe"
+  },
+  {
+    id: "vintage-tshirts",
+    name: "T-shirts & Sweats Vintage",
+    apiUrl: "https://api-vinted-fake.com/search?category=tshirts-sweats&style=vintage&regions=Europe&brands=surf,skate,Hawaii,USA,Australie,Indonésie"
+  },
+  {
+    id: "dr-martens",
+    name: "Chaussures Dr Martens",
+    apiUrl: "https://api-vinted-fake.com/search?category=chaussures&brand=DrMartens&region=Europe"
+  }
+];
